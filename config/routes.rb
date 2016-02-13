3c093f99913f3eaa8e5f3f78e39fd6cad8d76204
hashimoto-attsu <m.hashimoto@attsu.co.jp>
2016-02-13 02:59:19 +0000

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
+  get 'home/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root to: "home#index"
+
 end
